8ada2f686594bffb956703d356fa018d45c3817f
Harsh Kukreja <harshkukreja99@gmail.com>
2020-04-06 14:02:00 +0530

    Displayed the questions

--- app/Question.php
+++ app/Question.php
@@ -20,4 +20,29 @@ class Question extends BaseModel
         $this->attributes['title'] = $title;
         $this->attributes['slug'] = Str::slug($title);
     }
+
+    //accessor method
+
+    public function getUrlAttribute(){
+        return "questions/{$this->id}";
+    }
+
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
+
+    public function getAnswersStylesAttribute()
+    {
+        if ($this->answers_count > 0)
+        {
+            if($this->best_answer_id)
+            {
+                return "has-best-answer";
+            }
+            return "answered";
+
+        }
+        return "unanswered";
+    }
 }
