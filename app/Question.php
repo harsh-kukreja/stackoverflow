3f793fadd5b504fda48579117769df155526d8dd
Harsh Kukreja <harshkukreja99@gmail.com>
2020-04-06 12:26:10 +0530

    Added questionsFactory with all the fields mapped with a user Added mutator for slug(imp)

--- app/Question.php
+++ app/Question.php
@@ -2,11 +2,22 @@
 
 namespace App;
 
+use Illuminate\Support\Str;
+
 class Question extends BaseModel
 {
     //We dont want user as the name
     public function owner()
     {
         return $this->belongsTo(User::class, 'user_id');
+    }
+
+    //Mutator, kyu use karre kyuki meko koi toh event chahye like apne case me title tha and slug is related to slug title: 'hi lraveel' slug 'hi-lraveel' iseleye relate karle
+
+//    The syntax is as followed abhi hamne isko call kiya iseleye apne toh apni responsibility hai dalne ki title ko
+    public function setTitleAttribute($title)
+    {
+        $this->attributes['title'] = $title;
+        $this->attributes['slug'] = Str::slug($title);
     }
 }
