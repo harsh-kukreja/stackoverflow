50901ed2b1968cf06927fc365326267bf039319e
Harsh Kukreja <harshkukreja99@gmail.com>
2020-04-07 13:49:35 +0530

    Added answers migration with realtion mapping

---
+++ app/Answer.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+class Answer extends BaseModel
+{
+    public function question(){
+        return $this->belongsTo(Question::class);
+    }
+
+    public function author(){
+        return $this->belongsTo(User::class,'user_id');
+    }
+}
