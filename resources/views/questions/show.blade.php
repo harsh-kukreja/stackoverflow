7dae6e04646579d45f2cd0ff0ecdaa31856cdbe8
Harsh Kukreja <harshkukreja99@gmail.com>
2020-04-08 12:01:50 +0530

    Aadded ui for votes and fav answer

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -13,7 +13,24 @@
                     <div class="card-footer">
 
                        <div class="d-flex justify-content-between">
-                           <div></div>
+                           <div class="d-flex">
+                               <div>
+                                   <a href="#" title="Up Vote" class="d-block text-dark text-center">
+                                       <i class="fa fa-caret-up fa-3x"></i>
+                                   </a>
+                                   <h4 class="text-dark m-0 text-center"> {{ $question->votes_count }}</h4>
+                                   <a href="#" title="down Vote" class="d-block text-dark text-center">
+                                       <i class="fa fa-caret-down fa-3x"></i>
+                                   </a>
+                               </div>
+
+                               <div class="ml-5 mt-2">
+                                   <a href="#" title="Favorite" class="d-block text-dark text-center">
+                                       <i class="fa fa-star fa-2x"></i>
+                                   </a>
+                                   <h4 class="text-dark m-0">45</h4>
+                               </div>
+                           </div>
                            <div class="d-flex flex-column">
                                <div class="text-muted flex-column">
                                    Asked: {{$question->created_date}}
@@ -26,49 +43,20 @@
                                        {{$question->owner->name}}
                                    </div>
                                </div>
-
-
                            </div>
                        </div>
                     </div>
                 </div>
 
-                <div class="card"></div>
 
             </div>
         </div>
 
-        <div class="row mt-4">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h3 class="m-0">{{$question->answer_count}} {{Str::plural('Answers',$question->answer_count)}}</h3>
-                    </div>
-                    <div class="card-body">
-                        @foreach($question->answers as $answer)
-                            {!! $answer->body !!}
-                            <div class="d-flex justify-content-between">
-                                <div></div>
-                                <div class="d-flex flex-column">
-                                    <div class="text-muted flex-column">
-                                        Asked: {{$answer->created_date}}
-                                    </div>
-                                    <div class="d-flex mb-2">
-                                        <div>
-                                            <img src="{{$answer->author->avatar}}" alt="{{$answer->author->name}}">
-                                        </div>
-                                        <div class="mt-2 ml-2">
-                                            {{$answer->author->name}}
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <hr>
-                        @endforeach
-
-                    </div>
-                </div>
-            </div>
-        </div>
+        @include('answers._index')
     </div>
 @endsection
+
+@section('styles')
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    @endsection
