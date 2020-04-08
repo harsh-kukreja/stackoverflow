45d08f76b708825933e2af1035af1268da9e3705
Harsh Kukreja <harshkukreja99@gmail.com>
2020-04-08 11:30:12 +0530

    Answers displayed

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -34,7 +34,9 @@ class RouteServiceProvider extends ServiceProvider
         //
         //key should be same as the route name
         Route::bind('slug', function ($slug){
-           return Question::where('slug',$slug)->firstOrFail();
+           return Question::with('answers.author')
+               ->where('slug',$slug)
+               ->firstOrFail();
         });
         parent::boot();
     }
